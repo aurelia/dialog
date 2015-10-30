e8da53622b763b7ab3f385951c9df094699446e1
pwkad <patrickwaltersc21@yahoo.com>
2015-10-30 11:13:12 -0500

    fix(corejs): remove additional reference
    Loading two versions of corejs was causing performance issues.

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ System.config({
     "aurelia-templating": "github:aurelia/templating@0.16.0",
     "babel": "npm:babel-core@5.8.25",
     "babel-runtime": "npm:babel-runtime@5.8.24",
-    "core-js": "npm:core-js@1.1.4",
+    "core-js": "npm:core-js@0.9.18",
     "text": "github:systemjs/plugin-text@0.0.2",
     "github:aurelia/binding@0.10.0": {
       "aurelia-metadata": "github:aurelia/metadata@0.9.0",
@@ -89,11 +89,6 @@ System.config({
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:core-js@0.9.18": {
-      "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "process": "github:jspm/nodelibs-process@0.1.2",
-      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
-    },
-    "npm:core-js@1.1.4": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "process": "github:jspm/nodelibs-process@0.1.2",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
