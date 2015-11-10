454b7a0102dc8ee1d6be4b1b96edaf4481c39c05
pwkad <patrickwaltersc21@yahoo.com>
2015-11-10 10:30:58 -0600

    chore(update): update to latest versions of libs
    Includes updating to core-js 1.2.6

--- test/unit/attach-focus.spec.js
+++ test/unit/attach-focus.spec.js
@@ -1,20 +1,22 @@
 import {AttachFocus} from '../../src/attach-focus';
 import {Container} from 'aurelia-dependency-injection';
-import {templatingEngine} from 'aurelia-templating';
-import {initialize, DOM} from 'aurelia-pal-browser';
+import {TemplatingEngine} from 'aurelia-templating';
+import {initialize} from 'aurelia-pal-browser';
+import {DOM} from 'aurelia-pal';
 
 let element = document.createElement('div');
 
 describe('modal gets focused when attached', () => {
-  var attachFocus,
-    container;
+  let attachFocus;
+  let container;
+  let templatingEngine;
 
   beforeEach(() => {
     initialize();
     container = new Container();
     container.registerInstance(DOM.Element, element);
     jasmine.clock().install();
-    templatingEngine.initialize(container);
+    templatingEngine = container.get(TemplatingEngine);
   });
 
   afterEach(() => {
@@ -22,7 +24,7 @@ describe('modal gets focused when attached', () => {
   });
 
   it('should call the focus method when attached', done => {
-    attachFocus = templatingEngine.createModelForUnitTest(AttachFocus);
+    attachFocus = templatingEngine.createViewModelForUnitTest(AttachFocus);
     spyOn(element, 'focus').and.callThrough();
     attachFocus.attached();
     jasmine.clock().tick(1);
