49931d173acc29070277db2bec16ee89d5c8fa1a
Rob Eisenberg <rob@bluespire.com>
2016-06-15 11:31:36 -0400

    chore(all): working on the build in stages

--- build/paths.js
+++ build/paths.js
@@ -1,20 +1,48 @@
 var path = require('path');
 var fs = require('fs');
 
+// hide warning //
+var emitter = require('events');
+emitter.defaultMaxListeners = 20;
+
 var appRoot = 'src/';
 var pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
 
-module.exports = {
+var paths = {
   root: appRoot,
   source: appRoot + '**/*.js',
   html: appRoot + '**/*.html',
-  tests: 'test/**/*.js',
+  style: 'styles/**/*.css',
   output: 'dist/',
-  sample: 'sample',
   doc:'./doc',
   e2eSpecsSrc: 'test/e2e/src/*.js',
   e2eSpecsDist: 'test/e2e/dist/',
   packageName: pkg.name,
+  useTypeScriptForDTS: false,
+  importsToAdd: [],
+  sort: true,
   styleSource: 'styles/input.less',
   styleOutput: 'styles'
 };
+
+paths.ignore = ['aurelia-dialog.js'];
+paths.files = [
+  'dialog-options.js',
+  'dialog-result.js',
+  'ai-dialog-body.js',
+  'ai-dialog-footer.js',
+  'ai-dialog-header.js',
+  'ai-dialog.js',
+  'attach-focus.js',
+  'lifecycle.js',
+  'dialog-controller.js',
+  'renderer.js',
+  'dialog-renderer.js',
+  'dialog-service.js',
+  'dialog-configuration.js',
+  'aurelia-dialog.js'
+  ].map(function(file){
+  return paths.root + file;
+});
+
+module.exports = paths;
