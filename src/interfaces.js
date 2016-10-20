676ee58753cee99fc1fb2043115c46e164818f66
StrahilKazlachev <strahil.kazlachev@gmail.com>
2016-10-20 22:59:53 +0300

    feat(dialog-service): refactor consolidate open methods in one

---
+++ src/interfaces.js
@@ -0,0 +1,21 @@
+interface DialogSettings {
+  viewModel: any;
+  view?: any;
+  model?: any;
+  lock?: boolean;
+  startingZIndex?: number;
+  centerHorizontalOnly?: boolean;
+  ignoreTransitions?: boolean;
+  position?: (modalContainer: Element, modalOverlay: Element) => void;
+}
+
+interface CloseDialogResult {
+  wasCancelled: boolean;
+  output?: any;
+}
+
+interface OpenDialogResult {
+  wasCancelled: boolean; // see aurelia/dialog#223
+  controller?: DialogController;
+  closeResult?: Promise<CloseDialogResult>;
+}
