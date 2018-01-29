c8cdb3ab22bce5efedb4255380ddad145e7e6946
Tim Fish <tim@timfishy.co.uk>
2018-01-29 12:59:30 +0100

    I love vscode format-on-save but it adds unnecessary changes to PRs....

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 // Karma configuration
 // Generated on Sun Aug 28 2016 19:03:27 GMT-0400 (Eastern Daylight Time)
 
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
